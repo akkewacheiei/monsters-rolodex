ad8f3e93cb067f628000cb71da0be0f85989067b
Akkewach Yodsomboon <akkewach@planforfit.com>
2020-09-16 13:14:34 +0700

    Updates

---
+++ precache-manifest.86273763dce76711c6ed66b0d6a0db2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e156bd6cf5a5e47523a36e741093cb0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9fe0f68f546f295842ee",
+    "url": "/monsters-rolodex/static/css/main.3ac74467.chunk.css"
+  },
+  {
+    "revision": "724ff5f7fbcfae15cd6c",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1df26df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe0f68f546f295842ee",
+    "url": "/monsters-rolodex/static/js/main.8134f5e4.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
